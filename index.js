96eccc0e07e293e50475b2199f993f5d562de7f9
Mu-An Chiou <me@muanchiou.com>
2016-07-16 10:09:38 -0700

    Build text editing menu to allow copy and paste

--- index.js
+++ index.js
@@ -6,9 +6,71 @@ const mb = menubar({
   'window-position': 'topRight'
 })
 const gs = require('electron').globalShortcut
+const Menu = require('menu')
 
 mb.on('ready', function () {
   gs.register("command+shift+'", function () {
     mb.window.isVisible() ? mb.hideWindow() : mb.showWindow()
   })
+
+  var menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 })
+
+const template = [
+  {
+    label: 'Mojibar',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'Command+Z',
+        selector: 'undo:'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+Command+Z',
+        selector: 'redo:'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'Command+X',
+        selector: 'cut:'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'Command+C',
+        selector: 'copy:'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'Command+V',
+        selector: 'paste:'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'Command+A',
+        selector: 'selectAll:'
+      },
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) { if (focusedWindow) focusedWindow.reload() }
+      },
+      {
+        label: 'Preferance',
+        accelerator: 'Command+,',
+        click: function () { mb.window.webContents.send('open-preference') }
+      },
+      {
+        label: 'Quit App',
+        accelerator: 'Command+Q',
+        selector: 'terminate:'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator: 'Alt+Command+I',
+        click: function () { mb.window.toggleDevTools() }
+      }
+    ]
+  }
+]
